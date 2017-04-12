964bf5fb4a583de3b2d4b7fda974641ce3158545
Admin <Admin>
2017-04-12 22:27:18 +0200

    Logo Animation

--- public/js/logo.js
+++ public/js/logo.js
@@ -40,30 +40,3 @@ function rotateCircles() {
   circle3Move(1000)
 
 }
-
-/*
-function rotateCircles() {
-  $('div[class*="circle"]').each(function() {
-    var that = this,
-    direction = ["-", "+"],
-    chosenDirection = direction[Math.floor(Math.random() * direction.length)],
-    speed = Math.floor((Math.random() * 300) + 100),
-    looper = setInterval(circleMove, 2000);
-
-    function circleMove() {
-      $(that).animate({
-        rotation: chosenDirection + '=' + speed
-      }, {
-        duration: 2000,
-        easing: 'linear',
-        step: function(now) {
-          $(that).css({
-            "transform": "rotate(" + now + "deg)"
-          });
-        }
-      });
-    }
-    circleMove();
-  });
-}
-*/
